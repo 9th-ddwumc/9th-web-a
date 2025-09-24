d578676ae5d26c6c7c5c63151f5cbe9450d7550d
이소연 <122617471+silviasylee03@users.noreply.github.com>
2025-09-24 14:06:44 +0900

    :sparkles: 컴포넌트 분리

--- week02/mission01/src/App.tsx
+++ week02/mission01/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Todo from './components/Todo';
+import TodoBefore from './components/TodoBefore';
 
 function App() {
   return <>
-    <Todo/>
+    <Todo />
+    <TodoBefore/>
   </>;
 }
 
