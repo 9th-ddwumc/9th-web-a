031965a6c09a0c69f56af59bef87df12276f24de
jinnu-dev <jinwoolian@gmail.com>
2025-09-26 02:07:19 +0900

    🐛 week02_lian_02

--- week02_mission2/src/App.tsx
+++ week02_mission2/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Lian's Drakmode</h1>
+      <h1>Lian's Darkmode</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
