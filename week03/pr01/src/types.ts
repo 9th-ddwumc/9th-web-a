0c3c93cebf47f0c8cd53c0df3e50957f5f1ba425
이소연 <122617471+silviasylee03@users.noreply.github.com>
2025-09-29 22:44:58 +0900

    :sparkles: week03_bbungya_pr01

--- week03/pr01/src/types.ts
+++ week03/pr01/src/types.ts
@@ -2,16 +2,16 @@
 
 import type { ReactNode, ComponentType } from 'react';
 
-// For Link.tsx
 export interface LinkProps {
   to: string;
   children: ReactNode;
 }
 
-// For Route.tsx
 export interface RouteProps {
-  path?: string; // Optional path property to satisfy the Route element's props
-  component: ComponentType<unknown>;
+//   path?: string; 
+//   component: ComponentType<unknown>;
+    path: string;
+    component: ComponentType<unknown>;
 }
 
 // For Router.tsx
