b14ff567ad6904abfd9d7ca31be12096f3eb1f03
jinnu-dev <jinwoolian@gmail.com>
2025-10-03 22:44:05 +0900

    ✨week3_lian_01

---
+++ week03_lian_01/src/App.tsx
@@ -0,0 +1,12 @@
+import MoviePage from "./pages/MoviePage";
+
+function App() {
+  // console.log(import.meta.env.VITE_TMDB_KEY);
+  return (
+    <div className='App'>
+      <MoviePage />
+    </div>
+  );
+}
+
+export default App;
