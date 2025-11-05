f6c3947459da890b4ce24696c60fdc74e3a8d3bd
hoyeon <hoyeon@ghdus.>
2025-11-05 21:36:36 +0900

    '실습0'

---
+++ 1/vite-project/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import App from './App.tsx'
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
+)
